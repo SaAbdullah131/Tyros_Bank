9a5fa9c02b42f9576343a75dee807ae4ae3baf83
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-02-12 01:57:55 +0600

    Checking valid user or not using js in client side but don't do it feature

---
+++ js/login.js
@@ -0,0 +1,32 @@
+
+// step-01:add click event handler with the submit button
+
+const submitButton = document.getElementById("btn-submit");
+submitButton.addEventListener("click",function(){
+    // step:02-> Get the email address inside the email input field.
+    // always remember to use .value to get text from the input field
+    const inputEmail= document.getElementById("user-email");
+    const email = inputEmail.value;
+    // step:03-> Get Password
+    // 3.a: set id on the html element
+    // 3.b-> get the element
+    // 3.c-. get the value from the element
+
+    const inputPassword= document.getElementById("user-password");
+    const password = inputPassword.value;
+    // checking weather it gets or not
+    //  console.log(inputEmailValue,inputPasswordValue);
+    /**
+     * ---------------
+     *  DANGER: DO NOT VERIFY EMAIL PASSWORD ON THE CLIENT SIDE
+     * -----------
+     */
+   // step:04 -> verify email and password and check weather valid user or not
+   
+   if(email==='tyrobee@newbie.com' && password ==="tyro@@1"){
+    console.log("Valid User");
+}else {
+    console.log("Invalid User");
+}
+
+})
