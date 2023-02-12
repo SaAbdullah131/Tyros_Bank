23a3475db1d99a7aaa0c84e0c2664fa19337e4e8
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-02-13 01:56:35 +0600

    withdraw and balance update and tyro's simple bank done

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,6 +4,9 @@
  * 2.5->Also make the sure to convert the input into a number by using praseFloat...
  * 3.Get previous withdraw total
  * 4.Calculate total withdraw amount
+ * 5.Set Total withdraw amount
+ * 
+ * 7.Clear the input
  */
 // step:01->
 const withdrawButton = document.getElementById("btn-withdraw");
@@ -11,15 +14,31 @@ const withdrawButton = document.getElementById("btn-withdraw");
 withdrawButton.addEventListener("click",function() {
     // step 2
     const withdrawInputField = document.getElementById("withdraw-amount");
-    withdrawInput = parseFloat(withdrawInputField.value);
-    // step 3
-
+    const  withdrawInput = parseFloat(withdrawInputField.value);
+    withdrawInputField.value=''; 
+    
+    if(isNaN(withdrawInput)){
+        alert("Please Provide Valid Number");
+        return ;
+    }
     const previousWithdrawTotalElement = document.getElementById("withdraw-total");
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalElement.innerText);
-    console.log(previousWithdrawTotal);
-    // step 4
-
+    
     const previousBalanceTotal = document.getElementById("balance-total");
     const previousBalance = parseFloat(previousBalanceTotal.innerText);
+
+    
+
+    if(withdrawInput > previousBalance) {
+        alert("No Sufficient Money for withdraw");
+        return;
+    }
+    const totalWithdrawAmount = previousWithdrawTotal + withdrawInput ;
+    previousWithdrawTotalElement.innerText = totalWithdrawAmount;
+
+    previousBalanceTotal.innerText = previousBalance -  withdrawInput;
+
+    // step 7
+  
    
 })
