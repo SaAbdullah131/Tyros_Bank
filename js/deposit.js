40469985c4a4d4e35b078e714e41e9efca8ec818
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-02-12 21:58:33 +0600

    deposite and balance updated

--- js/deposit.js
+++ js/deposit.js
@@ -8,14 +8,26 @@ depositButton.addEventListener("click",function(){
 
        // step:02-> Get the deposit amount from  input field
     const depositAmountArea = document.getElementById("deposit-amount");
-    const depositAmount = depositAmountArea.value;
+    const newDepositAmountString= depositAmountArea.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+    
     // step:03-> Get the current deposit total
-    
     // for non-input{element other than input} use innnerText to get the text
-    const depositTotalElement = document.getElementById("total-deposit");
 
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText= depositAmount;
-    
+    const depositTotalElement = document.getElementById("total-deposit");
+     const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal =parseFloat(depositTotalElement.innerText);
+
+    // step:04-> Add numbers to set the total deposit
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
+    // step::05-> Get the balance current total
+    const previousBalanceTotalEl = document.getElementById("balance-total");
+    const previousBalanceTotal = parseFloat(previousBalanceTotalEl.innerText);
+    const currentTotalBalance = previousBalanceTotal + newDepositAmount;
+    // step:06-> the balance total
+    previousBalanceTotalEl.innerText = currentTotalBalance;
+    //step:07-> clear the deposit field
+    depositAmountArea.value=' ';
 
 })
